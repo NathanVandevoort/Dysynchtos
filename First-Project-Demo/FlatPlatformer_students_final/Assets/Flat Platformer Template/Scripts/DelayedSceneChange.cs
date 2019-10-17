3176841da6ddb169e7d717afbce3157dadc35ccd
NathanVandevoort <nathan.vandevoort@digipen.edu>
2019-10-17 13:16:14 -0700

    work?

--- First-Project-Demo/FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/DelayedSceneChange.cs
+++ First-Project-Demo/FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/DelayedSceneChange.cs
@@ -14,4 +14,24 @@ public class DelayedSceneChange : MonoBehaviour
     public string NextScene = "GameOver";
 
 
+    //function will change scene after a specified delay when run
+    public void ChangeScene()
+    {
+        print("Started");
+        StartCoroutine(DelayedChange());
+    }
+
+    IEnumerator DelayedChange()
+    {
+        print("before delay");
+
+        yield return new WaitForSeconds(Delay);
+
+        print("after");
+        SceneManager.LoadScene(NextScene);
+    }
+
+
+
+
 }
